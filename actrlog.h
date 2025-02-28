09c411b56496dc44fbccd591472f8fec14b17828
Ethan <105807243+Snambo1@users.noreply.github.com>
2025-02-27 23:04:14 -0500

    Update actrlog.h

--- actrlog.h
+++ actrlog.h
@@ -7,6 +7,6 @@ extern void _actr_log_length(const char * pointer, int size);
 /// @brief used for debugging, log messages will appear in the browser console
 /// @param pointer 
 void actr_log(const char * pointer) {
-    _actr_log_length(pointer, strlen(pointer));
+    _actr_log_length(pointer, actr_strlen(pointer));
 }
 #endif
